6c6f8c7c65281c36dd6d7f52e178612cd0708bbd
Laísa C. de Castro <laisa.castro@hotmail.com>
2016-04-19 12:36:14 -0300

    adicionando aviso caso tente realizar review

--- app/src/main/java/com/example/laisa/tcc/ListSRActivity.java
+++ app/src/main/java/com/example/laisa/tcc/ListSRActivity.java
@@ -2,12 +2,14 @@ package com.example.laisa.tcc;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,6 +20,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.laisa.JWTUtil.JwtToken;
+import com.example.laisa.Type.StageType;
 import com.example.laisa.Util;
 import com.example.laisa.entidades.SystematicReview;
 import com.google.gson.Gson;
@@ -91,6 +94,12 @@ public class ListSRActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        srlist = new ArrayList<>();
+        listSR = (ListView) findViewById(R.id.listViewListSR);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,srlist);
+        listSR.setAdapter(adapter);
+        listSR.setOnItemClickListener(onItemClickListener);
+        adapter.notifyDataSetChanged();
         Util.getEmail(getApplicationContext());
         ListSRTask listSRTask = new ListSRTask();
         listSRTask.execute();
@@ -152,9 +161,22 @@ public class ListSRActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             SystematicReview sr = (SystematicReview) listSR.getItemAtPosition(position);
-            Intent i = new Intent(ListSRActivity.this,ReadSRActivity.class);
-            i.putExtra("systematicReview",sr);
-            startActivity(i);
+            if(sr.getStage().equals(StageType.FINAL_REVIEW)){
+                new AlertDialog.Builder(ListSRActivity.this)
+                        .setTitle("Not yet implemented")
+                        .setMessage("We apologize but the review stage hasn't been implemented yet")
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .show();
+            }else {
+                Intent i = new Intent(ListSRActivity.this, ReadSRActivity.class);
+                i.putExtra("systematicReview", sr);
+                startActivity(i);
+            }
         }
     };
 
